1dd4fe980f210cf6a70a2bbfd17e3454a550c573
TakenakaEri <takenakaeri0423@gmail.com>
2025-08-30 00:12:54 +0900

    アバターURLを使用するように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   has_many :events, through: :participations
   
   validates :name, presence: true
+
+ # GitHub Pages + GitHub リポジトリを使用してアバターを表示する
+  def avatar_url
+    base_url = "https://あなたのユーザー名.github.io/bbq-app-assets/avatars"
+    
+    avatar_mapping = {
+      "田中 太郎" => "tanaka",
+      "佐藤 花子" => "sato",
+      "山田 次郎" => "yamada",
+      "鈴木 美咲" => "suzuki",
+      "渡辺 健一" => "watanabe"
+    }
+    
+    filename = avatar_mapping[name] || "default"
+    "#{base_url}/#{filename}.png"
+  end
 end
